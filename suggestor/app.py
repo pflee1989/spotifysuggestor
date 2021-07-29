65b4f5851be894beec1cf16054bbf84e77e27ac5
Philip Lee <pflee1989@outlook.com>
2021-07-29 14:04:15 -0400

    make it better

--- suggestor/app.py
+++ suggestor/app.py
@@ -1,14 +1,13 @@
 import pandas as pd
 from flask import Flask, render_template, request
-from .models import song_model, to_list
+from .models import build_model, to_list
 
-
-df = pd.read_csv('suggestor/US.csv')
-
+df = pd.read_csv('suggestor/clean_US.csv')
+#       <img src="static/images/App_Logo.jpg" style="max-width:200px;height:auto;">
 def create_app():
     app = Flask(__name__)
-
-    @app.route("/", methods = ["GET", "POST"])
+    @app.route("/", methods=["GET", "POST"])
+    
     def main_page():
         """
         1. Asks user for their name.
@@ -18,28 +17,29 @@ def create_app():
         if request.method == "GET":
             return render_template('home.html')
         if request.method == "POST":
-            return render_template('greet.html', name=request.form.get("name", "you"))
-        
-    @app.route("/music", methods = ["GET", "POST"]) 
+            return render_template('greet.html',
+                                   name=request.form.get("name", "you"))
+            
+    @app.route("/music", methods=["GET", "POST"])
     def input():
-        """ 
+        """
         Input user's favorite song.
         Return list of recommended songs.
         """
         if request.method == "GET":
             track_artist = to_list(df)
             return render_template('input_song.html', data=track_artist)
-        
         if request.method == "POST":
             input = request.form.get("input_song")
             index = df.loc[df.isin([input]).any(axis=1)].index.tolist()
             index = index[0]
-            model = song_model(index)
-            return render_template('output_song.html', output_song=input, recommended_song=model)
-    
-    @app.route("/about") 
+            model = build_model(index)
+            return render_template('output_song.html',
+                                   output_song=input, recommended_song=model)
+
+    @app.route("/about")
     def about():
-        """ 
+        """
         Our about us page.
         """
         return render_template('about.html')
--- suggestor/models.py
+++ suggestor/models.py
@@ -4,44 +4,41 @@ from sklearn.neighbors import NearestNeighbors
 
 
 
-def song_model(input):
+def build_model(input):
     """
     Nearest Neighbors Model for song suggestions
     """
-    df = pd.read_csv('suggestor/US.csv')
-    # adding direct url to data set by adding url prefix and id
-    url = 'http://open.spotify.com/track/' + df['id']
-    df['url'] = url
-    
-    # reordering columns, leaving out ID and release date
-    df = df[['combined', 'url', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy',
-       'explicit', 'instrumentalness', 'key', 'liveness', 'loudness',
-       'mode', 'popularity', 'speechiness', 'tempo', 'valence']]
-    
-   # target set will be both artist and name
-    y_set = ['combined', 'url']
-
-    # droping target from data matrix
-    df_data = df.drop(y_set, axis=1)
-
-    # set target
-    df_target = df[y_set]
-
-    # fit on data, 12 neighbors
-    nn = NearestNeighbors(algorithm='brute', metric='cosine', leaf_size =15, n_neighbors=12, n_jobs=-1)
-    nn.fit(df_data)
-
-    # sample a song(index) from df_data to use as our query point 
-    input_index=input
-
-    # vectorize 
-    data_vect = [df_data.iloc[input_index].values]
-    neigh_dist, neigh_indices = nn.kneighbors(data_vect)
-    indexs = neigh_indices.flat[0:12].tolist()
-
-    #adding url to each track
-    output = df_target.iloc[indexs]
+    df = pd.read_csv('suggestor/clean_US.csv')
+    # configure song index Nearest Neighbor result output
+    mask = ['combined', 'url']
+    df_result = df[mask]
+    # set up data DataFrame for numerical features only
+    data = df.select_dtypes('number')
+    def normalize_column(col):
+        """
+        function to normalize data in numerical features
+        """
+        max_d = data[col].max()
+        min_d = data[col].min()
+        data[col] = (data[col] - min_d)/(max_d - min_d)
+    num_types = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    num = data.select_dtypes(include=num_types)
+    for col in num.columns:
+        normalize_column(col)
+    # nearest neighbors configuration
+    knn = NearestNeighbors(algorithm='ball_tree',
+                           radius=1.75, leaf_size =60, metric='euclidean', n_neighbors=12, n_jobs=-1)
+    knn.fit(data)
+    # query point to sample from
+    input_index = input
+    # vectorize data for processing and cast to a list
+    data_vector = [data.iloc[input_index].values]
+    nn_distance, nn_indices = knn.kneighbors(data_vector)
+    indices = nn_indices.flat[0:7].tolist()
+    # generate output
+    output = df_result.iloc[indices]
     return (output)
+
 
 def to_list(df):
     """
